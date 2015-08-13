484c189110280187d337279a3c662c8e7815d1a6
Ikuo Nagira <nagi@enagi.co.jp>
2015-08-13 10:29:20 +0900

    初期化用CSS追加

--- functions.php
+++ functions.php
@@ -12,9 +12,10 @@ function mnc_wp_enqueue_scripts() {
   
   wp_dequeue_style('main-css');
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array('base-css') );
-  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/css/style.css', array( 'parent-style' ) );
+  wp_enqueue_style( 'child-base-style', get_stylesheet_directory_uri() . '/base.css', array('parent-style'));
+  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/css/style.css', array( 'parent-style', 'child-base-style' ) );
   wp_enqueue_script( 'wideslider', get_stylesheet_directory_uri() . '/js/wideslider.js', array('jquery'), '', true );
-  wp_enqueue_style( 'chil-main-style', get_stylesheet_uri(), array( 'child-style' ) );
+  wp_enqueue_style( 'chil-main-style', get_stylesheet_uri(), array( 'child-style', 'child-base-style' ) );
   wp_enqueue_script('jquery-ui-tabs');
   wp_enqueue_script('jquery-easing', '//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js', array('jquery'));
   wp_enqueue_script('tab', get_stylesheet_directory_uri() . '/js/tab.js', array('jquery'));
