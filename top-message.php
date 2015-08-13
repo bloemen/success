472635e78d2e023ac39e976b58e60aaf9097d417
Ikuo Nagira <nagi@enagi.co.jp>
2015-08-13 18:52:14 +0900

    Revert "#top-slideは複数あるため、.descriptionに変更"
    This reverts commit 5cefc8776ad2bcf4814ead100290a095944cce83.

--- top-message.php
+++ top-message.php
@@ -1,5 +1,4 @@
 <div id="top-slide">
-<div class="description">
 <?php $messagePost = get_posts(array('name'=>'message', 'post_type'=>'page')); ?>
 <?php if (count($messagePost)>0): ?>
 <p class="ex_ttl">
@@ -9,5 +8,4 @@
 <?php echo $messagePost[0]->post_content;?>
 </p>
 <?php endif;?>
-	</div>
 </div>
