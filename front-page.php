b1439c032a954661d881717bb71589074d72850f
Ikuo Nagira <nagi@enagi.co.jp>
2015-08-13 22:29:48 +0900

    Attention表示のスタイル適用

--- front-page.php
+++ front-page.php
@@ -1,24 +1,15 @@
 <?php get_header(); ?>
-
-<?php echo get_template_part('top','slide'); ?>
-
 <div id="content">
-
-<div class="wrap">
-    
   <div id="main" <?php bzb_layout_main(); ?> role="main" itemprop="mainContentOfPage" itemscope="itemscope" itemtype="http://schema.org/Blog">
-
-
-
-
+    <article id="content_top_block" class="grid_space">
+        <?php echo get_template_part('top','slide'); ?>
         <?php echo get_template_part('top','message'); ?>
         <?php echo get_template_part('top','attention'); ?>
+    </article>
         <?php echo get_template_part('top','newposts'); ?>
         <?php echo get_template_part('top','freedownload'); ?>
         <?php echo get_template_part('top','category'); ?>
         <?php echo get_template_part('top','sns'); ?>
-
-
   </div><!-- /main -->
   
 
