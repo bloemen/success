f929ae91b69958c5e43a8aea74484555bc71c324
Ikuo Nagira <nagi@enagi.co.jp>
2015-09-03 17:00:59 +0900

    ヘッダー・フッダーのロゴにサイトトップへのリンクを追加

--- footer.php
+++ footer.php
@@ -3,32 +3,33 @@
 <div class="footer_inner">
 <section class="wrapper clearfix">
 <div class="pagetop"><a href="#pagetop">PAGETOP</a></div>
-<div class="f_logo"><img src="<?php echo get_stylesheet_directory_uri();?>/images/logo_f.png" width="306" height="155" alt="成功の法則" /></div>
-<?php if( has_nav_menu( 'footer_nav' ) ){ ?>
+<div class="f_logo"><a href="<?php echo home_url();?>"><img src="<?php echo get_stylesheet_directory_uri();?>/images/logo_f.png" width="306" height="155" alt="成功の法則" /></a></div>
+<?php if (has_nav_menu('footer_nav')) {
+	?>
         <?php
-        wp_nav_menu(
-          array(
-            'theme_location'  => 'footer_nav',
-            'menu_class'      => 'list_categories',
-            'menu_id'         => '',
-            'container'       => 'ul',
-            'container_id'      => '',
-            'container_class' => ''
-          )
-          ); ?>
+wp_nav_menu(
+		array(
+			'theme_location' => 'footer_nav',
+			'menu_class' => 'list_categories',
+			'menu_id' => '',
+			'container' => 'ul',
+			'container_id' => '',
+			'container_class' => '',
+		)
+	);?>
         <ul style="display: none;">
    <?php
-        wp_nav_menu(
-          array(
-            'theme_location'  => 'footer_nav',
-            'menu_class'      => 'list_categories',
-            'menu_id'         => '',
-            'container'       => 'ul',
-            'container_id'      => '',
-            'container_class' => ''
-          )
+wp_nav_menu(
+		array(
+			'theme_location' => 'footer_nav',
+			'menu_class' => 'list_categories',
+			'menu_id' => '',
+			'container' => 'ul',
+			'container_id' => '',
+			'container_class' => '',
+		)
 
-         );?>
+	);?>
          </ul>
          </section>
     </div><!-- /wrap -->
@@ -39,35 +40,37 @@
 
   <div  id="bottom" class="bottom">
     <section class="wrapper" sytle="">
-        © Copyright <?php echo date('Y'); ?> <?php echo get_bloginfo('name'); ?>. All rights reserved.
+        © Copyright <?php echo date('Y');?> <?php echo get_bloginfo('name');?>. All rights reserved.
     </section><!-- /wrap -->
   </div><!-- /footer-02 -->
   <?php
-  // }
-  ?>
-<?php wp_footer(); ?>
+// }
+?>
+<?php wp_footer();?>
 <script>
 (function($){
 
 $(function(){
-  <?php if( !wp_is_mobile() ){?>
+  <?php if (!wp_is_mobile()) {?>
   $(".sub-menu").css('display', 'none');
   $("#gnav-ul li").hover(function(){
     $(this).children('ul').fadeIn('fast');
   }, function(){
     $(this).children('ul').fadeOut('fast');
   });
-  <?php }?>
+  <?php }
+?>
   // スマホトグルメニュー
-  
-  <?php if( is_front_page() ){ ?>
+
+  <?php if (is_front_page()) {?>
     $('#gnav').addClass('active');
-  <?php }else{ ?>
+  <?php } else {?>
     $('#gnav').removeClass('active');
-    
-  <?php } ?>
-  
-  
+
+  <?php }
+?>
+
+
   $('#header-menu-tog a').click(function(){
     $('#gnav').toggleClass('active');
   });
--- header.php
+++ header.php
@@ -2,13 +2,13 @@
 <html lang="ja">
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
 	<meta charset="UTF-8">
-	<title><?php bzb_title(); ?></title>
+	<title><?php bzb_title();?></title>
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
 
-<?php wp_head(); ?>
+<?php wp_head();?>
 
 <script>
 jQuery(function($){
@@ -27,85 +27,89 @@ $(function(){
 
 </head>
 
-<body id="#top" <?php body_class( 'wrapper_body' );?> itemschope="itemscope" itemtype="http://schema.org/WebPage">
+<body id="#top" <?php body_class('wrapper_body');?> itemschope="itemscope" itemtype="http://schema.org/WebPage">
   <a id="pagetop" name="pagetop"></a>
-<?php bzb_show_facebook_block(); ?>
+<?php bzb_show_facebook_block();?>
 
-<?php if( is_singular('lp') ) { ?>
+<?php if (is_singular('lp')) {?>
 
 <div class="lp-wrap">
 
 <header id="lp-header">
-  <h1 class="lp-title"><?php wp_title(''); ?></h1>
+  <h1 class="lp-title"><?php wp_title('');?></h1>
 </header>
 
-<?php }else{ ?>
+<?php } else {
+	?>
 
 <header id="header" role="banner" itemscope="itemscope" itemtype="http://schema.org/WPHeader" class="header">
   <section class="wrapper clearfix">
   <div class="box_first">
   <div class="wrapper_metadata">
     <?php
-      $logo_image = get_option('logo_image');
-      $logo_text = get_option('logo_text');
-      if( !empty($logo_image) && get_option('toppage_logo_type') == 'logo_image') :
-        $logo_inner = '<img src="'. get_option('logo_image') .'" alt="'.get_bloginfo('name').'" />';
-      else:
-        if (!empty($logo_text) && get_option('toppage_logo_type') == 'logo_text') :
-          $logo_inner = get_option('logo_text');
-        else:
-          $logo_inner = get_bloginfo('name');
-        endif;
-      endif;
-      $logo_wrap = ( is_front_page() || is_home() ) ? 'h1' : 'p' ;
-    ?>
-      <<?php echo $logo_wrap; ?> id="logo" itemprop="headline" class="title_website">
-        <a href="javascript:void(0)" rel="<?php echo home_url(); ?>"><?php echo $logo_inner; ?></a>
-      </<?php echo $logo_wrap; ?>>
+$logo_image = get_option('logo_image');
+	$logo_text = get_option('logo_text');
+	if (!empty($logo_image) && get_option('toppage_logo_type') == 'logo_image'):
+		$logo_inner = '<img src="' . get_option('logo_image') . '" alt="' . get_bloginfo('name') . '" />';
+	else:
+		if (!empty($logo_text) && get_option('toppage_logo_type') == 'logo_text'):
+			$logo_inner = get_option('logo_text');
+		else:
+			$logo_inner = get_bloginfo('name');
+		endif;
+	endif;
+	$logo_wrap = (is_front_page() || is_home()) ? 'h1' : 'p';
+	?>
+      <<?php echo $logo_wrap;?> id="logo" itemprop="headline" class="title_website">
+        <a href="<?php echo home_url();?>" rel="<?php echo home_url();?>"><?php echo $logo_inner;?></a>
+      </<?php echo $logo_wrap;?>>
     </div>
     </div>
   </section>
   <?php bzb_header_social_buttons();?>
 
-<?php if( has_nav_menu( 'global_nav' ) ){ ?>
+<?php if (has_nav_menu('global_nav')) {?>
         <div id="header-menu-tog"> <a href="#"><i class="fa fa-align-justify"></i></a></div>
-<?php }?>
+<?php }
+	?>
 
 <!-- start global nav  -->
 <!---->
-<?php if( has_nav_menu( 'global_nav' ) ){ ?>
+<?php if (has_nav_menu('global_nav')) {
+		?>
 <div id="gnav" role="navigation" itemscope="itemscope" itemtype="http://scheme.org/SiteNavigationElement">
   <section class="wrapper clearfix" style="display:none">
   <ul>
   <?php
-    wp_nav_menu(
-      array(
-        'theme_location'  => 'global_nav',
-        'menu_class'      => 'list_categories',
-        'menu_id'         => '',
-        'container'       => 'ul',
-        'container_id'    => '',
-        'container_class' => ''
-      )
-    );?>  
+wp_nav_menu(
+			array(
+				'theme_location' => 'global_nav',
+				'menu_class' => 'list_categories',
+				'menu_id' => '',
+				'container' => 'ul',
+				'container_id' => '',
+				'container_class' => '',
+			)
+		);?>
   </ul>
   </section>
   <section class="wrapper clearfix">
   <ul>
   <?php
-    wp_nav_menu(
-      array(
-        'theme_location'  => 'global_nav',
-        'menu_class'      => 'list_categories',
-        'menu_id'         => '',
-        'container'       => 'ul',
-        'container_id'    => '',
-        'container_class' => ''
-      )
-    );?>
+wp_nav_menu(
+			array(
+				'theme_location' => 'global_nav',
+				'menu_class' => 'list_categories',
+				'menu_id' => '',
+				'container' => 'ul',
+				'container_id' => '',
+				'container_class' => '',
+			)
+		);?>
   </ul>
     </section>
 </div>
-<?php } ?>
+<?php }
+	?>
   </header>
 <?php } // if is_singular('lp') ?>
